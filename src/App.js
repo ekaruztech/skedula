8769fb564b9d25e5e8dbf972a9ada47059c5be8c
nerd35 <umukorochinedu@gmail.com>
2019-12-11 19:00:25 +0100

    develop section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Login from './components/login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <h1>Welcome to Skedula App</h1>
+     <Login/>
     </div>
   );
 }
