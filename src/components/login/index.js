94876eb3865411ade9f2a78a754e4e05e76df557
nerd35 <umukorochinedu@gmail.com>
2019-12-12 14:46:48 +0100

    feat(src/components/login):Login section added

---
+++ src/components/login/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "../element/Button";
+
+function Login() {
+  return (
+    <div>
+      <h1>This is Login</h1>
+      <Button>Login</Button>
+    </div>
+  );
+}
+
+export default Login;
