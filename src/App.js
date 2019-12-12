31d9164b120e32627202cf8aaa4fc3c0500adcbf
A.Adegoke Jnr <adeg4ife@gmail.com>
2019-12-12 09:03:37 +0100

    feat(App.js): router added for login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import Login from './components/login';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./components/login";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-     <h1>Welcome to Skedula App</h1>
-     <Login/>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Welcome to Skedula App</h1>
+        <Route exact path="/" component={Login} />
+      </div>
+    </Router>
   );
 }
 
