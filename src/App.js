e1aba08269dd1f859769f602cbd41fb4ab1d1776
Simeon Akpanudo <akpanudosimeon@gmail.com>
2019-12-13 11:52:41 +0100

    fix(src/App.js): fixed merge issue

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
-
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "./components/Auth";
-
 import "./App.css";
 
 function App() {
