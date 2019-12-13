38c060d4a38e60c79fb0a729dc0ef04518760a32
Simeon Akpanudo <akpanudosimeon@gmail.com>
2019-12-13 11:47:31 +0100

    fix(all): fix merge conflict

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "./components/Auth";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./components/login";
+
 import "./App.css";
 
 function App() {
@@ -11,6 +16,11 @@ function App() {
           <Auth />
         </div>
       </Switch>
+
+      <div className="App">
+        <h1>Welcome to Skedula App</h1>
+        <Route exact path="/" component={Login} />
+      </div>
     </Router>
   );
 }
