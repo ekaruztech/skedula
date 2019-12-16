4514e6388aeeb5ca1453030b6b2d5dfb1ef91842
nerd35 <umukorochinedu@gmail.com>
2019-12-16 16:08:31 +0100

    feat(src):added register page

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,15 +1,50 @@
 import React from "react";
-import Button from "../element/Button";
-import InputFieldLogin from "./InputFieldLogin";
+import { Container, Row, Col } from "reactstrap";
+import Logo from "../assets/images/Skedula.svg";
+import "./Login.scss";
+import InputField from "../elements/InputField";
+import Button from "../elements/Button";
 
-function Login() {
+const Login = props => {
   return (
-    <div>
-      <h1>This is Login</h1>
-      <InputFieldLogin />
-      <Button>Login</Button>
-    </div>
+    <Container className="themed-container " fluid={true}>
+      <Row className="mt-4">
+        <Col xs="12" lg="6" className="mt-5">
+          <div className="text-center">
+            <img src={Logo} alt="logo" />
+          </div>
+          <p className="text-title text-center">
+            A lightweight task management and todo app
+          </p>
+          <form>
+            <div className="input-group mb-2 w-75 mt-4 mx-auto">
+              <div className="input-group-prepend ">
+                <div className="input-group-text">
+                  <i className="fas fa-user text-secondary"></i>
+                </div>
+              </div>
+              <InputField type={"text"} placeholder="Username" />
+            </div>
+            <div className="input-group mb-2 w-75 mt-4 mx-auto">
+              <div className="input-group-prepend ">
+                <div className="input-group-text">
+                  <i className="fas fa-lock text-secondary"></i>
+                </div>
+              </div>
+              <InputField type={"password"} placeholder="Password" />
+            </div>
+            <div className="ml-5 mt-4">
+              <Button variant="primary" size={"lg"}>
+                Login
+              </Button>
+              <h4>Forgot password?</h4>
+            </div>
+          </form>
+        </Col>
+        <Col xs="6">.col-6</Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default Login;
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,11 +1,71 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import Logo from "../assets/images/Skedula.svg";
+import "./Register.scss";
+import InputField from "../elements/InputField";
+import Button from "../elements/Button";
 
-function Register() {
+const Register = props => {
   return (
-    <div>
-      <h1>Hello Register</h1>
-    </div>
+    <Container className="themed-container ">
+      <Row>
+        <Col xs="12" lg="6">
+          <div className="text-center">
+            <img src={Logo} alt="logo" />
+          </div>
+          <p className="text-title text-center">
+            A lightweight task management and todo app
+          </p>
+          <form>
+            <div className="form-group mb-2 w-75 mt-3 mx-auto">
+              <div className="input-group-prepend ">
+                <div className="input-group-text iconStyle">
+                  <i className="fas fa-envelope"></i>
+                </div>
+                <InputField
+                  type={"email"}
+                  placeholder="email@yourcompany.com"
+                />
+              </div>
+            </div>
+            <div className="form-group mb-2 w-75 mt-3 mx-auto">
+              <div className="input-group-prepend ">
+                <div className="input-group-text iconStyle">
+                  <i className="fas fa-user"></i>
+                </div>
+                <InputField type={"text"} placeholder="Username" />
+              </div>
+            </div>
+            <div className="form-group mb-2 w-75 mt-3 mx-auto">
+              <div className="input-group-prepend ">
+                <div className="input-group-text iconStyle">
+                  <i className="fas fa-lock"></i>
+                </div>
+                <InputField type={"password"} placeholder="Password" />
+              </div>
+            </div>
+            <div className="form-group mb-2 w-75 mt-3 mx-auto">
+              <div className="input-group-prepend ">
+                <div className="input-group-text iconStyle">
+                  <i className="fas fa-lock"></i>
+                </div>
+                <InputField type={"password"} placeholder="Confirm password" />
+              </div>
+            </div>
+            <Row>
+              <Col className="ml-3">
+                <Button variant="primary" size={"md"} style=" px-4 p-1">
+                  Register
+                </Button>
+              </Col>
+              <p className="mr-5"> Already have an Account? </p>
+            </Row>
+          </form>
+        </Col>
+        <Col xs="6">.col-6</Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default Register;
