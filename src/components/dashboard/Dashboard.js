55dfd3490e09e8c7d999ad1107ce8273d30245d6
Simeon Akpanudo <akpanudosimeon@gmail.com>
2019-12-17 12:28:27 +0100

    chore(src/components/dashboard/Dashboard.js): adds sidenav component to dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Sidenav from "./sideNav";
+import { Container, Row, Col } from "reactstrap";
 
 function Dashboard() {
   return (
-    <div>
-      <h1>Hello Dashboard</h1>
-    </div>
+    <Container className="themed-container" fluid={true}>
+      <Row>
+        <Sidenav />
+      </Row>
+    </Container>
   );
 }
 
