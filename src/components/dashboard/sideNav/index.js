ee94c2b3e5bf8b50fc21d87ea2d1cccfb5ebdabd
Simeon Akpanudo <akpanudosimeon@gmail.com>
2019-12-17 14:25:04 +0100

    feat(src/components/dashboard/sideNav): added feat for empty todolist

--- src/components/dashboard/sideNav/index.js
+++ src/components/dashboard/sideNav/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, Row, Col, Collapse } from "reactstrap";
+import { Container, Row, Col, Collapse, Button } from "reactstrap";
 import "./styles/SideNav.scss";
 import { ReactComponent as Logo } from "../../assets/images/Skedula_white.svg";
 
@@ -56,21 +56,21 @@ function List(props) {
 function Sidenav() {
   const [isOpen, setOpen] = useState(false);
   const [todos, setTodo] = useState([
-    {
-      title: "A trip to france",
-      id: 5,
-      current: true
-    },
-    {
-      title: "John's wedding ceremony",
-      id: 3,
-      current: false
-    },
-    {
-      title: "IPO plans",
-      id: 8,
-      current: false
-    }
+    // {
+    //   title: "A trip to france",
+    //   id: 5,
+    //   current: true
+    // },
+    // {
+    //   title: "John's wedding ceremony",
+    //   id: 3,
+    //   current: false
+    // },
+    // {
+    //   title: "IPO plans",
+    //   id: 8,
+    //   current: false
+    // }
   ]);
   return (
     <Col xs="3" className="Sidenav p-0 m-0">
@@ -84,43 +84,56 @@ function Sidenav() {
               dropdown={true}
               text={"My todos"}
               iconTwo={<i className="fa fa-chevron-down" />}
-              iconOne={<i class="fas fa-briefcase"></i>}
+              iconOne={<i className="fas fa-briefcase"></i>}
               onClick={e => setOpen(prev => !prev)}
             />
             <Collapse isOpen={isOpen}>
               <div className="Dropdown w-100">
-                {todos.map(todo => (
-                  <DropdownItem
-                    key={todo.id}
-                    text={todo.title}
-                    current={todo.current}
-                  />
-                ))}
+                {todos.length < 1 ? (
+                  <div className="emptyTodo w-100">
+                    <div className="display-5">
+                      <strong>You do not have a todo yet</strong>
+                    </div>
+                    <div className="empty-btn">
+                      <Button variant="primary" className="add-todo-side-nav">
+                        Create todo
+                      </Button>
+                    </div>
+                  </div>
+                ) : (
+                  todos.map(todo => (
+                    <DropdownItem
+                      key={todo.id}
+                      text={todo.title}
+                      current={todo.current}
+                    />
+                  ))
+                )}
               </div>
             </Collapse>
             <List
               dropdown={false}
               text={"Pending"}
-              iconOne={<i class="far fa-circle"></i>}
+              iconOne={<i className="far fa-circle"></i>}
             />
 
             <List
               dropdown={false}
               text={"In Progress"}
-              iconOne={<i class="fas fa-spinner"></i>}
+              iconOne={<i className="fas fa-spinner"></i>}
             />
             <List
               dropdown={false}
               text={"Completed"}
               iconTwo={<i className="fa fa-chevron-down" />}
-              iconOne={<i class="fas fa-check"></i>}
+              iconOne={<i className="fas fa-check"></i>}
             />
           </div>
           <div className="logout-container w-100">
             <List
               dropdown={false}
               text={"Logout"}
-              iconOne={<i class="fas fa-sign-out-alt"></i>}
+              iconOne={<i className="fas fa-sign-out-alt"></i>}
             />
           </div>
         </Row>
